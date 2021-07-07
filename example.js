0ef3926a4ff752359ebcaba2aacf93a9dd9b4652
Filip Chrisffersson <flpchrsn@gmail.com>
2021-07-07 09:25:26 +0200

    Update example.js

--- example.js
+++ example.js
@@ -2,8 +2,9 @@ const Booli = require('./index');
 const TYPES = require('./lib/types');
 
 const main = async () => {
-  // Get area code for Olof Palmes gata
+  // Get area code for Midsommarkransen and Stockholm
   const areas = await Booli.areas.getAreas(['Midsommarkransen', 'Stockholm']);
+  // Get the first area id
   const areaId = areas.areas[0].booliId;
 
   // Get listings for the area code
